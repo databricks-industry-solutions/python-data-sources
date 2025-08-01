efaf6b15e93dd0db5cd3c13f58c09488f77d15a0
Douglas Moore <dmoore247@users.noreply.github.com>
2025-07-31 22:56:32 -0400

    ...

--- zipdcm/db_runner.py
+++ zipdcm/db_runner.py
@@ -5,6 +5,8 @@ import os
 
 sys.dont_write_bytecode = True
 
+print( os.path.abspath('.') )
+
 result = pytest.main(['-vvv', '--import-mode=importlib', '.'])
 if result != 0:
     raise Exception(f"Tests failed with exit code: {result}")
